dbb18a0c76ac57932e62c29ae03be846ad67207f
Vegard Bendiktsen <glitchdetector@gmail.com>
2019-11-09 21:37:09 +0100

    Update boilerplate.lua
    Added support for highlighting sidebar button
    Added easy sidebar badge controls

--- webadmin-lua/server/included/boilerplate.lua
+++ webadmin-lua/server/included/boilerplate.lua
@@ -1,26 +1,37 @@
+-- Page details
 local PAGE_NAME = "boilerplate"
 local PAGE_TITLE = "Boilerplate Plugin"
 local PAGE_ICON = "plug"
 
+-- Sidebar badge controls
+local SHOW_PAGE_BADGE = true
+local PAGE_BADGE_TEXT = "OK!"
+local PAGE_BADGE_TYPE = "success"
+
 function CreatePage(FAQ, data, add)
     add(FAQ.Alert("info", "Boilerplate is working!"))
     add(FAQ.InfoCard("Random Facts", {
-        {"Lines of code", 29},
-        {"Characters", 1161},
+        {"Lines of code", 40},
+        {"Characters", 1622},
         {"Created", "November 7th 2019"},
+        {"Updated", "November 9th 2019"},
     }))
     return true, "OK"
 end
 
 -- Automatically sets up a page and sidebar option based on the above configurations
+-- This should not need to be altered, and serves as the foundation of the plugin
 Citizen.CreateThread(function()
+    local PAGE_ACTIVE = false
     local FAQ = exports['webadmin-lua']:getFactory()
     exports['webadmin']:registerPluginOutlet("nav/sideList", function(data) --[[R]]--
         if not exports['webadmin']:isInRole("webadmin."..PAGE_NAME..".view") then return "" end
-        return FAQ.SidebarOption(PAGE_NAME, PAGE_ICON, PAGE_TITLE) --[[R]]--
+        local _PAGE_ACTIVE = PAGE_ACTIVE PAGE_ACTIVE = false
+        return FAQ.SidebarOption(PAGE_NAME, PAGE_ICON, PAGE_TITLE, SHOW_PAGE_BADGE and PAGE_BADGE_TEXT or false, PAGE_BADGE_TYPE, _PAGE_ACTIVE) --[[R]]--
     end)
     exports['webadmin']:registerPluginPage(PAGE_NAME, function(data) --[[E]]--
         if not exports['webadmin']:isInRole("webadmin."..PAGE_NAME..".view") then return "" end
+        PAGE_ACTIVE = true
         return FAQ.Nodes({ --[[R]]--
             FAQ.PageTitle(PAGE_TITLE),
             FAQ.BuildPage(CreatePage, data), --[[R]]--
